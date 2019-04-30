d32b52c6e4247a8b9495bf5732543a2025a91435
Dane Kilpatrick <danekilpatrick@Danes-MacBook-Air.local>
2019-04-30 16:38:52 -0500

    Started working on the first iteration of css for the section The-Latest

---
+++ main.js
@@ -0,0 +1,7 @@
+// console.log("this is just a test.");
+// document.getElementById("header-title").style.color = 'red';
+// document.getElementById('header-title').style.background = 'purple';
+
+
+const myHeading = document.getElementById('header-title');
+myHeading.addEventListener('click', );
