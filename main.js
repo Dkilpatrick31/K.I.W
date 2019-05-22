ed103b872cac59a014fa8bed2ca6b8635cbf99db
Dane Kilpatrick <danekilpatrick@Danes-MacBook-Air.local>
2019-05-22 16:02:25 -0500

    Added a few more images and started working on the From this week's Issue Section

--- main.js
+++ main.js
@@ -1,7 +1,31 @@
-// console.log("this is just a test.");
-// document.getElementById("header-title").style.color = 'red';
-// document.getElementById('header-title').style.background = 'purple';
+'use strict';
 
+let one = "" + 1 + 0;
+let two = "" - 1 + 0
+let three = true + false
+let four = 6 / "3"
+let five = "2" * "3"
+let six = 4 + 5 + "px"
+let seven = "$" + 4 + 5
+let eight = "4" - 2
+let nine = "4px" - 2
+let ten = 7 / 0
+let eleven = "  -9  " + 5
+let twelve = "  -9  " - 5
+let thirtenn = null + 1
+let fourteen = undefined + 1
 
-const myHeading = document.getElementById('header-title');
-myHeading.addEventListener('click', );
+console.log(Number(one));
+console.log(String(two));
+console.log(String(three));
+console.log(String(four));
+console.log(String(five));
+console.log(String(six));
+console.log(String(seven));
+console.log(String(eight));
+console.log(String(nine));
+console.log(String(ten));
+console.log(String(eleven));
+console.log(String(twelve));
+console.log(String(thirteen));
+console.log(String(fourteen));
